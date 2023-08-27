6ac243b4974c9d3c4c5225cf5124fa469f02336c
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2023-08-27 12:22:38 -0300

    Adição das constantes da fonte Roboto!!!

---
+++ kratos/constant/fonts.js
@@ -0,0 +1,22 @@
+/*
+    As constantes da fonte ROBOTO foram todas retiradas do Google Fonts.
+    Consulte: https://fonts.google.com/specimen/Roboto
+
+    O ChatGPT foi de grande ajuda para fazer todo esse código sem a
+    necessidade de trabalho manual e repetitivo.
+*/
+
+const fontPath = "kratos/assets/fonts/Roboto/";
+
+export const ROBOTO = new Font(`${fontPath}Roboto-Regular.ttf`)
+export const ROBOTO_BOLD = new Font(`${fontPath}Roboto-Bold.ttf`)
+export const ROBOTO_ITALIC = new Font(`${fontPath}Roboto-Italic.ttf`)
+export const ROBOTO_BOLD_ITALIC = new Font(`${fontPath}Roboto-BoldItalic.ttf`)
+export const ROBOTO_LIGHT = new Font(`${fontPath}Roboto-Light.ttf`)
+export const ROBOTO_LIGHT_ITALIC = new Font(`${fontPath}Roboto-LightItalic.ttf`)
+export const ROBOTO_MEDIUM = new Font(`${fontPath}Roboto-Medium.ttf`)
+export const ROBOTO_MEDIUM_ITALIC = new Font(`${fontPath}Roboto-MediumItalic.ttf`)
+export const ROBOTO_THIN = new Font(`${fontPath}Roboto-Thin.ttf`)
+export const ROBOTO_THIN_ITALIC = new Font(`${fontPath}Roboto-ThinItalic.ttf`)
+export const ROBOTO_BLACK = new Font(`${fontPath}Roboto-Black.ttf`)
+export const ROBOTO_BLACK_ITALIC = new Font(`${fontPath}Roboto-BlackItalic.ttf`)
