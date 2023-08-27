b289cee8599587e660cd4b127f29b7674255e6d2
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2023-08-27 12:23:54 -0300

    Rascunho inicial do núcleo da aplicação!!!

--- kratos/Kratos.js
+++ kratos/Kratos.js
@@ -1,1 +1,19 @@
-console.log("Hello World!!!")
+import DarkTheme from "./theme/DarkTheme.js"
+
+export class Kratos {
+    constructor(body, theme) {
+        this.body = body
+        this.theme = theme
+    }
+
+    run() {
+        while (true) {
+            Screen.clear(this.theme.backgroundColor)
+            Screen.flip()
+        }
+    }
+}
+
+export default function (body = [], theme = DarkTheme()) {
+    return new Kratos(body, theme)
+}
